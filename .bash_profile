0548f9d16d8e1556e5e72d17f64c3d56d9bac846
dawid.kublik@vimn.com <dawid.kublik@vimn.com>
2017-05-11 13:38:28 +0200

    Initial Commit

---
+++ .bash_profile
@@ -0,0 +1,4 @@
+export GRADLE_HOME="/Users/kublikd/programs/gradle-2.13"
+export MONGO_HOME="/usr/local/mongodb/"
+export TOOLS_HOME="/Users/kublikd/tools/"
+export PATH=$PATH:$GRADLE_HOME/bin:$MONGO_HOME/bin:$TOOLS_HOME
---
+++ tools/run-service.sh
@@ -0,0 +1,26 @@
+base_dir=~
+
+service_name=$1
+echo '----- running ' service_name
+service_homes=( "$base_dir/projects/arc-ng" "$base_dir/projects/arc-ng2" )
+
+for service_home in "${service_homes[@]}"
+do
+   echo 'checking location' $service_home
+   maybe_service_dir="$service_home"/"$service_name"
+   if [ -d "$maybe_service_dir" ]
+      then
+	 service_dir=$maybe_service_dir
+         break
+      else
+	:
+   fi
+done
+
+echo '--- building ' $service_dir
+
+cd $service_dir
+./gradlew build
+./gradlew -Dspring.profiles.active=dev-local bootRun
+
+
